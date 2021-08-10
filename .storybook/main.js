c5ddf7098779a2c1dd336decd288f90f968f36f6
김응길 <yesroad.dev@gmail.com>
2021-08-10 10:46:28 +0900

    타입스크립스 설정 추가

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,20 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app"
-  ]
+  ],
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      compilerOptions: {
+        allowSyntheticDefaultImports: false,
+        esModuleInterop: false,
+      },
+    }
+  }  
 }
