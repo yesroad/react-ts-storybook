ef9e769c17f54ff1591ed3b21608f1e86c65ac81
김응길 <yesroad.dev@gmail.com>
2021-08-11 13:02:39 +0900

    스토리북 환경 다시 세팅

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,20 +1,11 @@
 module.exports = {
-  stories: [
+  "stories": [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  addons: [
+  "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app"
-  ],
-  typescript: {
-    reactDocgen: 'react-docgen-typescript',
-    reactDocgenTypescriptOptions: {
-      compilerOptions: {
-        allowSyntheticDefaultImports: false,
-        esModuleInterop: false,
-      },
-    }
-  }  
+  ]
 }
