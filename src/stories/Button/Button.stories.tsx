52b6620c195c2e80bfd890037d0809689a537fd0
김응길 <yesroad.dev@gmail.com>
2021-08-11 15:10:49 +0900

    argTypes 추가

--- src/stories/Button/Button.stories.tsx
+++ src/stories/Button/Button.stories.tsx
@@ -3,36 +3,51 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import Button from './Button';
 
 export default {
-  title: 'Component/Button',
-  component: Button,
+	title: 'Component/Button',
+	component: Button,
+	argTypes: {
+		theme: {
+			control: { type: 'select' },
+			options: ['primary', 'secondary', 'tertiary'],
+		},
+		size: {
+			control: { type: 'select' },
+			options: ['small', 'medium', 'large'],
+		},
+	},
 } as ComponentMeta<typeof Button>;
 
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 
+const defaultArgs: {} = {
+	disabled: false,
+	size: 'medium',
+};
+
 export const Default = Template.bind({});
 Default.args = {
-  theme: 'primary',
-  children: 'Button',
-  disabled: false,
+	theme: 'primary',
+	children: 'Button',
+	...defaultArgs,
 };
 
 export const Primary = Template.bind({});
 Primary.args = {
-  theme: 'primary',
-  children: 'Primary',
-  disabled: false
+	theme: 'primary',
+	children: 'Primary',
+	...defaultArgs,
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-  theme: 'secondary',
-  children: 'Secondary',
-  disabled: false
+	theme: 'secondary',
+	children: 'Secondary',
+	...defaultArgs,
 };
 
 export const Tertiary = Template.bind({});
 Tertiary.args = {
-  theme: 'tertiary',
-  children: 'Tertiary',
-  disabled: false
+	theme: 'tertiary',
+	children: 'Tertiary',
+	...defaultArgs,
 };
--- src/stories/Button/types.d.ts
+++ src/stories/Button/types.d.ts
@@ -2,11 +2,11 @@ export interface ButtonProps {
   /** 버튼 안의 내용 */
   children: React.ReactNode;
   /** 버튼 테마 */
-  theme: 'primary' | 'secondary' | 'tertiary';
+  theme?: 'primary' | 'secondary' | 'tertiary';
   /** 버튼 사이즈 */
   size?: 'small' | 'medium' | 'large';
   /** 클릭했을 때 호출할 함수 */
   onClick?: () => void;
   /** 버튼을 비활성화 */
-  disabled?: boolean;
+  disabled: boolean;
 }
