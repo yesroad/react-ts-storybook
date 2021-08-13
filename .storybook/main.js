8dd1fc2f4e02205a56a95d7b592950fe61ceaf46
김응길 <yesroad.dev@gmail.com>
2021-08-13 14:10:52 +0900

    이모션 프라그먼트 제거 바벨 추가

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,5 +7,23 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app"
-  ]
+  ],
+  babel: async () => ({
+    plugins: [
+      [
+        "@emotion/babel-plugin-jsx-pragmatic",
+        {
+          "export": "jsx",
+          "import": "__to_avoid_prepending_/** @jsxImportSource @emotion/react */",
+          "module": "@emotion/react"
+        }
+      ],
+      [
+        "@babel/plugin-transform-react-jsx",
+        {
+          "pragma": "__to_avoid_prepending_/** @jsxImportSource @emotion/react */"
+        }
+      ],
+    ]
+  }),  
 }
